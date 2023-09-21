975718ff5cd506bbfc0a8eaede7a095af049af90
alexmvo1997 <140145610+alexmvo1997@users.noreply.github.com>
2023-09-21 17:56:41 -0500

    Ex1_a_vo.m

--- Ex1_a_vo.m
+++ Ex1_a_vo.m
@@ -1,7 +1,7 @@
-name = 'vo'; % ---- Step 2 ---- variable 'name' created equal to last name
-n = size(name,2); % ---- Step 3 ---- variable "n" created equal to length of variable 'name'
-z = (randi([1 3],3,1)) * n % ---- Step 4 & 5 ---- generates 3x1 vector "z" and prints on screen
-[a,b,c] = deal(z(1,1),z(2,1),z(3,1)); % ---- Step 6 ---- assign "z" values to variables "a","b", and "c"
-x1 = real(-b+sqrt(b^2-(4*a*c)))/(2*a); % ---- Step 7 ---- assign variables "x1" and "x2" to quadratic formula
+name = 'vo'; % variable 'name' created equal to last name
+n = size(name,2); % variable "n" created equal to length of variable 'name'
+z = (randi([1 3],3,1)) * n % generates 3x1 vector "z" and prints on screen
+[a,b,c] = deal(z(1,1),z(2,1),z(3,1)); % assign "z" values to variables "a","b", and "c"
+x1 = real(-b+sqrt(b^2-(4*a*c)))/(2*a); % assign variables "x1" and "x2" to quadratic formula
 x2 = real(-b-sqrt(b^2-(4*a*c)))/(2*a);
-fprintf(1,'The two roots are x1 = %.2f and x2 = %.2f',x1,x2) % ---- Step 8 ---- roots given in a printed statement
+fprintf(1,'The two roots are x1 = %.2f and x2 = %.2f',x1,x2) %  roots given in a printed statement
